f900f2eef3ac456e7de14ae23c9aefc480945b01
Hossin <ha222yp@student.lnu.se>
2022-01-08 22:13:13 +0100

    office controller ready

--- src/main/java/com/retail/demo/controllers/OfficeController.java
+++ src/main/java/com/retail/demo/controllers/OfficeController.java
@@ -1,14 +1,18 @@
 package com.retail.demo.controllers;
 
+import com.retail.demo.models.Employee;
 import com.retail.demo.models.Office;
+import com.retail.demo.models.Product;
 import com.retail.demo.services.OfficeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/office")
 public class OfficeController {
 
@@ -20,13 +24,26 @@ public class OfficeController {
     }
 
     @GetMapping("/list-all")
-    public List<Office> getAllOffices() {
-        return new ArrayList<>(this.officeService.getAllOffices());
+    public String getAllOffices(Model model) {
+        List<Office> offices = this.officeService.getAllOffices();
+        String title = "All offices";
+
+        model.addAttribute("offices", offices);
+        model.addAttribute("title", title);
+        return "/office/office-list";
     }
 
     @GetMapping("/{id}")
-    public Office getOfficeById(@PathVariable Integer id) {
-        return this.officeService.findById(id);
+    public String getOfficeById(Model model,@PathVariable Integer id) {
+        Office office = null;
+        try {
+            office  = officeService.findById(id);
+            model.addAttribute("allowDelete", false);
+        } catch (Exception ex) {
+            model.addAttribute("errorMessage", "No office found with that ID");
+        }
+        model.addAttribute("office", office);
+        return "/office/office";
     }
 
     @GetMapping("/city")
@@ -35,34 +52,102 @@ public class OfficeController {
     }
 
     @GetMapping("/list-all/{country}")
-    public List<Office> getAllOfficesByCountry(@PathVariable String country) {
-        return new ArrayList<>(this.officeService.findByCountry(country));
+    public String getAllOfficesByCountry(Model model,@PathVariable String country) {
+        List<Office> offices = this.officeService.findByCountry(country);
+        String title = "All offices in:" + country;  ///////////////////////
+
+        model.addAttribute("title", title);
+        model.addAttribute("offices", offices);
+
+        return "/office/offices-list";
+    }
+
+    @GetMapping("/add-office")
+    public String getAddOffice(Model model) {
+        Office office = new Office();
+        model.addAttribute("add", true);
+        model.addAttribute("office", office);
+
+        return "office/update";
     }
 
     @PostMapping("/add-office")
-    public void addOffice(@RequestBody Office office) {
+    public String addOffice(Model model,@ModelAttribute("office") Office office) {
         try {
-            this.officeService.save(office);
-        } catch (Exception e) {
-            e.printStackTrace();
+            Office newOffice = officeService.save(office);
+            return "redirect:/office/" + newOffice.getOfficeCode();
+        } catch (Exception ex) {
+            String errorMessage = ex.getMessage();
+            model.addAttribute("errorMessage", errorMessage);
+
+            model.addAttribute("add", true);
+            return "/office/update";
         }
     }
 
-    @PutMapping("/update-office")
-    public void updateOffice(@RequestBody Office office) {
+    @GetMapping("/update-office/{id}")
+    public String getUpdateProduct(Model model, @PathVariable Integer id) {
+        Office office = null;
         try {
-            this.officeService.update(office);
-        } catch (Exception e) {
-            e.printStackTrace();
+            office = officeService.findById(id);
+        } catch (Exception ex) {
+            model.addAttribute("errorMessage", ex.getMessage());
+        }
+        model.addAttribute("add", false);
+        model.addAttribute("office", office);
+        return "/office/update";
+    }
+
+    @PutMapping("/update-office/{id}")
+    public String updateOffice(Model model,
+                               @PathVariable Integer id,
+                               @ModelAttribute("office") Office office) {
+        try {
+            office.setOfficeCode(id);
+            officeService.update(office);
+            return "redirect:/office/" + office.getOfficeCode();
+        } catch (Exception ex) {
+            String errorMessage = ex.getMessage();
+            model.addAttribute("errorMessage", errorMessage);
+
+            model.addAttribute("add", false);
+            return "/office/update";
         }
     }
 
-    @DeleteMapping("/delete-office/{id}")
-    public void deleteOffice(@PathVariable Integer id) {
+    @GetMapping("/delete-office/{id}")
+    public String getDeleteOffice(Model model,
+                                   @PathVariable Integer id) {
+        Office office = null;
         try {
-            this.officeService.deleteById(id);
-        } catch (Exception e) {
-            e.printStackTrace();
+            office = officeService.findById(id);
+        } catch (Exception ex) {
+            model.addAttribute("errorMessage", ex.getMessage());
+        }
+        model.addAttribute("allowDelete", true);
+        model.addAttribute("office", office);
+        return "/office/office";
+    }
+
+    @PostMapping("/delete-office/{id}")
+    public String deleteOffice(Model model,
+                                @PathVariable Integer id) {
+        try {
+            officeService.deleteById(id);
+            return "redirect:/office/list-all";
+        } catch (Exception ex) {
+            String errorMessage = ex.getMessage();
+            model.addAttribute("errorMessage", errorMessage);
+            return "/office/office";
         }
     }
+
+   // @DeleteMapping("/delete-office/{id}")
+   // public void deleteOffice(@PathVariable Integer id) {
+   //     try {
+   //         this.officeService.deleteById(id);
+   //     } catch (Exception e) {
+   //         e.printStackTrace();
+   //     }
+   // }
 }
