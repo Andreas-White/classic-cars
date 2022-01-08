90b4f029b50c1b48f5828bcd7a9fa069cfdb2a53
Hossin <ha222yp@student.lnu.se>
2022-01-08 18:33:49 +0100

    product edit-del done

--- src/main/java/com/retail/demo/controllers/ProductController.java
+++ src/main/java/com/retail/demo/controllers/ProductController.java
@@ -86,22 +86,77 @@ public class ProductController {
         }
     }
 
-
-    @PutMapping("/update-product")
-    public void updateProduct(@RequestBody Product product) {
+    @GetMapping("/update-product/{id}")
+    public String getUpdateProduct(Model model, @PathVariable String id) {
+        Product product = null;
         try {
-            this.productService.update(product);
-        } catch (Exception e) {
-            e.printStackTrace();
+            product = productService.getProductById(id);
+        } catch (Exception ex) {
+            model.addAttribute("errorMessage", ex.getMessage());
+        }
+        model.addAttribute("add", false);
+        model.addAttribute("product", product);
+        return "/product/update";
+    }
+
+    @PostMapping("/update-product/{id}")
+    public String processUpdateProduct(Model model,
+                                        @PathVariable String id,
+                                        @ModelAttribute("customer") Product product) {
+        try {
+            product.setProductCode(id);
+            productService.update(product);
+            return "redirect:/product/" + product.getProductCode();
+        } catch (Exception ex) {
+            String errorMessage = ex.getMessage();
+            model.addAttribute("errorMessage", errorMessage);
+
+            model.addAttribute("add", false);
+            return "/product/update";
         }
     }
 
-    @DeleteMapping("/delete-product/{id}")
-    public void deleteProduct(@PathVariable String id) {
+    @GetMapping("/delete-product/{id}")
+    public String getDeleteProduct(Model model,
+                                    @PathVariable String id) {
+        Product product = null;
         try {
-            this.productService.deleteById(id);
-        } catch (Exception e) {
-            e.printStackTrace();
+            product = productService.getProductById(id);
+        } catch (Exception ex) {
+            model.addAttribute("errorMessage", ex.getMessage());
+        }
+        model.addAttribute("allowDelete", true);
+        model.addAttribute("product", product);
+        return "/product/product";
+    }
+
+    @PostMapping("/delete-product/{id}")
+    public String deleteProduct(Model model,
+                                 @PathVariable String id) {
+        try {
+            productService.deleteById(id);
+            return "redirect:/product/list-all";
+        } catch (Exception ex) {
+            String errorMessage = ex.getMessage();
+            model.addAttribute("errorMessage", errorMessage);
+            return "/product/product";
         }
     }
+ // @PutMapping("/update-product")
+ // public void updateProduct(@RequestBody Product product) {
+ //     try {
+ //         this.productService.update(product);
+ //     } catch (Exception e) {
+ //         e.printStackTrace();
+ //     }
+ // }
+
+ // @DeleteMapping("/delete-product/{id}")
+ // public void deleteProduct(@PathVariable String id) {
+ //     try {
+ //         this.productService.deleteById(id);
+ //     } catch (Exception e) {
+ //         e.printStackTrace();
+ //     }
+ // }
 }
